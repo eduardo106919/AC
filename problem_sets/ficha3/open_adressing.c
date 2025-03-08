143dbaa3b47afca50c96b17d99cd719335643c80
eduardo106919 <a106919@alunos.uminho.pt>
2025-03-08 19:52:18 +0000

    added: hash tables, closed adressing and open adressing

---
+++ problem_sets/ficha3/open_adressing.c
@@ -0,0 +1,129 @@
+#include <stdio.h>
+
+#define Size 100
+
+#define Free 0
+#define Used 1
+#define Del 2
+
+typedef struct bucket {
+    int status; // Free | Used | Del
+    char *chave; int ocorr;
+} THash [Size];
+
+/* hash function */
+unsigned hash(char *str){
+    unsigned hash = 5381;
+    int c;
+
+    while (c = *str++) {
+        /* hash * 33 + c */
+        hash = ((hash << 5) + hash) + c;
+    }
+
+    return hash;
+}
+
+/* finds the index in which s is, or should be */
+int where (char *s, THash t) {
+    unsigned index = hash(s) % Size;
+
+    unsigned i;
+    // find s or the first non used position
+    for (i = 0; i < Size && t[index].status == Used && strcmp(t[index].chave, s) != 0; i++) {
+        index = (index + 1) % Size;
+    }
+
+    unsigned pos = Size;
+    if (i < Size) {
+        // first non used position
+        pos = index;
+        if (t[index].status != Used) {
+
+            // search for s
+            for (i = 0; i < Size && t[index].status != Free && strcmp(t[index].chave, s) != 0; i++) {
+                index = (index + 1) % Size;
+            }
+
+            // found s
+            if (i < Size && t[index].status == Used)
+                pos = index;
+        }
+    }
+
+    return pos;
+}
+
+/* initializes the hash table with empty positions */
+void initEmpty (THash t) {
+    for (unsigned i = 0; i < Size; i++)
+        t[i].status = Free;
+}
+
+/* adds an ocorrence of a word */
+void add (char *s, THash t) {
+    unsigned index = where(s, t);
+
+    if (index < Size) {
+        if (t[index].status == Used)
+            t[index].ocorr++;
+        else {
+            t[index].status = Used;
+            t[index].ocorr = 1;
+            t[index].chave = s;
+        }
+    }
+}
+
+/* searchs for s in the table */
+int lookup (char *s, THash t) {
+    unsigned index = where(s, t);
+
+    if (index < Size && t[index].status == Used)
+        return t[index].ocorr;
+
+    return 0;
+}
+
+/* removes an ocurrence of s */
+int removeT (char *s, THash t) {
+    unsigned index = where(s, t);
+
+    if (index < Size && t[index].status == Used) {
+        t[index].ocorr--;
+        if (t[index].ocorr == 0)
+            t[index].status = Del;
+
+        return 0;
+    }
+
+    return 1;
+}
+
+/* cleans the table */
+int garb_collection (THash t) {
+    THash temp;
+    unsigned i = 0, index = 0;
+
+    for (i = 0; i < Size; i++) {
+        temp[i].status = Free;
+    }
+
+    for (i = 0; i < Size; i++) {
+        if (t[i].status == Used) {
+            index = where(t[i].chave, temp);
+
+            temp[i].status = Used;
+            temp[i].ocorr = t[i].ocorr;
+            temp[i].chave = t[i].chave;
+        }
+    }
+
+    for (i = 0; i < Size; i++) {
+        t[i].status = temp[i].status;
+        t[i].ocorr = temp[i].ocorr;
+        t[i].chave = temp[i].chave;
+    }
+
+    return 0;
+}
